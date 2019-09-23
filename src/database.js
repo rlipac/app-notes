368a75ea686ba716f6acda4c1b830cc704c2249e
Richard Lipa <rlipac31@gmail.com>
2019-09-23 11:55:35 -0500

    subiendo app-notes

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notes-db-app', {
+mongoose.connect('mongodb+srv://richard:mariabelen@cluster0-pagkr.mongodb.net/test?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
      useUnifiedTopology: true,
     useFindAndModify: false
 })
-    .then( db => console.log('DB is Connected'))
+    .then( db => console.log('DB is Connected...exito!!!'))
     .catch( err => console.error(err));
