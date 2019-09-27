128f016fba2413cd228b805285824f022f396cd0
Richard Lipa <rlipac31@gmail.com>
2019-09-27 12:47:12 -0500

    aumentando candidados

---
+++ src/eleccion.js
@@ -0,0 +1,23 @@
+function votoPresi1() {
+    var votacion = document.getElementById("voto1").value;
+    document.getElementById("mivoto").value = votacion;
+    alert("holaaa");
+}
+function votoPresi2() {
+    var votacion = document.getElementById("voto2").value;
+    document.getElementById("mivoto").value = votacion;
+
+}
+function votoPresi3() {
+    var votacion = document.getElementById("voto3").value;
+    document.getElementById("mivoto").value = votacion;
+}
+function votoPresi4() {
+    var votacion = document.getElementById("voto4").value;
+    document.getElementById("mivoto").value = votacion;
+
+}
+function votoPresi5() {
+    var votacion = document.getElementById("voto5").value;
+    document.getElementById("mivoto").value = votacion;
+}
